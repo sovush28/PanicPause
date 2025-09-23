1c04bb55063467808d8b1c4bdac9d3bea7db3899
Yulia Ignateva <yuliya.ignateva.06@mail.ru>
2025-09-23 15:42:38 +0500

    страницы входа и регистрации внешний вид

--- app/src/main/java/com/example/safespace/SignInActivity.java
+++ app/src/main/java/com/example/safespace/SignInActivity.java
@@ -13,27 +13,30 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class LoginActivity extends AppCompatActivity implements View.OnClickListener {
+import org.w3c.dom.Text;
 
-    EditText emailET, passwET;
-    TextView forgotPasswTV, createAccTV;
-    Button logInBtn;
+public class SignInActivity extends AppCompatActivity implements View.OnClickListener {
+
+    EditText emailET, newPasswET, codeET;
+    TextView logInTV;
+    Button getCodeBtn, signInBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_login);
+        setContentView(R.layout.activity_sign_in);
 
-        emailET=(EditText)findViewById(R.id.login_email_et);
-        passwET=(EditText)findViewById(R.id.login_passw_et);
-        forgotPasswTV=(TextView)findViewById(R.id.forgot_passw_tv);
-        createAccTV=(TextView)findViewById(R.id.create_acc_tv);
-        logInBtn=(Button)findViewById(R.id.log_in_btn);
+        emailET=(EditText)findViewById(R.id.signin_email_et);
+        newPasswET=(EditText)findViewById(R.id.signin_passw_et);
+        codeET=(EditText)findViewById(R.id.code_et);
+        logInTV=(TextView)findViewById(R.id.to_login_tv);
+        getCodeBtn=(Button)findViewById(R.id.get_code_btn);
+        signInBtn=(Button)findViewById(R.id.sign_in_btn);
 
-        logInBtn.setOnClickListener(this);
-        forgotPasswTV.setOnClickListener(this);
-        createAccTV.setOnClickListener(this);
+        logInTV.setOnClickListener(this);
+        getCodeBtn.setOnClickListener(this);
+        signInBtn.setOnClickListener(this);
 
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
@@ -44,15 +47,11 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
 
     @Override
     public void onClick(View v){
-        if(v.getId()==R.id.log_in_btn){
-            Intent intent=new Intent(this, MainActivity.class);
+        if(v.getId()==R.id.to_login_tv){
+            Intent intent=new Intent(this, LoginActivity.class);
             startActivity(intent);
             overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out); // Плавное появление/исчезание
             finish();
-        }
-        if(v.getId()==R.id.create_acc_tv){
-            Intent intent=new Intent(this, SignInActivity.class);
-            startActivity(intent);
         }
     }
 }
